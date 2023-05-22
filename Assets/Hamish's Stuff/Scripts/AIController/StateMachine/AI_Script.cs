ab7ca52da403f0c7fc0a77c78e3d88ae3f7104f5
Hamish Luke <61484269+Amatorii@users.noreply.github.com>
2023-05-22 18:23:16 +1000

    Added Scene for State Machine testing / Update scene for playtesting

---
+++ Assets/Hamish's Stuff/Scripts/AIController/StateMachine/AI_Script.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Script : MonoBehaviour
+{
+    [SerializeField] private GameObject _player; //gives the Companion a target to follow
+
+    [SerializeField] private StateMachine _stateMachine;
+    public float _dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
