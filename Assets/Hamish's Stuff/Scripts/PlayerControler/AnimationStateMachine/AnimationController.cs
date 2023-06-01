a1516f66e6ea86857e620af7a8ff6d83c0ce2d03
Hamish Luke <hamishrluke@gmail.com>
2023-06-01 14:11:33 +1000

    Finished Animation

--- Assets/Hamish's Stuff/Scripts/PlayerControler/AnimationStateMachine/AnimationController.cs
+++ Assets/Hamish's Stuff/Scripts/PlayerControler/AnimationStateMachine/AnimationController.cs
@@ -10,7 +10,8 @@ namespace Amatorii_Controller
     {
         private PlayerController1 _p;
         private Animator _anim;
-        private bool _facingRight = false;
+        [SerializeField] private bool _facingRight = true;
+        [SerializeField]private bool _moving = false;
 
         private void Start()
         {
@@ -20,10 +21,39 @@ namespace Amatorii_Controller
 
         void Update()
         {
+            if(_p.Input.X == -1)
+            {
+                _facingRight = false;
+            }
+            else if(_p.Input.X == 1)
+            {
+                _facingRight = true;
+            }
+            else
+            {
+                _moving = false;
+            }
+
+            if(_p._currentVerticalSpeed < 0)
+            {
+                StartCoroutine(FallingAnimation());
+                return;
+            }
+
+            if (_p.JumpingThisFrame)
+            {
+                StartCoroutine(JumpAnimation());
+            }
+
             if (_p.Grounded)
             {
                 if(_p.Input.X != 0)
                 {
+                    if (_moving)
+                    {
+                        RunLoop();
+                        return;
+                    }
                     StartCoroutine(RunAnimation(_p.Input.X));
                     return;
                 }
@@ -37,6 +67,44 @@ namespace Amatorii_Controller
             }
         }
 
+        private void RunLoop()
+        {
+            if (_p.Input.X == 0)
+            {
+                _moving = false;
+                return;
+            }
+            if (_facingRight)
+                _anim.Play("metarig_Character_RunningLoop_Right");
+            else
+                _anim.Play("metarig_Character_RunningLoop_Left");
+        }
+
+        private IEnumerator JumpAnimation()
+        {
+            if (_facingRight)
+                _anim.Play("metarig_Character_Jump_Right");
+            else
+                _anim.Play("metarig_Character_Jump_Left");
+
+            float animationLength = _anim.GetCurrentAnimatorStateInfo(0).length;
+
+
+            yield return new WaitForSeconds(animationLength);
+        }
+
+        private IEnumerator FallingAnimation()
+        {
+            if (_facingRight)
+                _anim.Play("metarig_Character_Falling_Right");
+            else
+                _anim.Play("metarig_Character_Falling_Left");
+            float animationLength = _anim.GetCurrentAnimatorStateInfo(0).length;
+
+
+            yield return new WaitForSeconds(animationLength);
+        }
+
         private IEnumerator RunAnimation(float i) //if I is negative, the player is facing left
         {
             if(i == -1)
@@ -45,9 +113,8 @@ namespace Amatorii_Controller
                     _anim.Play("metarig_Character_Switch_RightLeft");
                 else
                     _anim.Play("metarig_Character_IdleRun_Left");
-                _facingRight = false;
             }
-            else
+            else if (i == 1)
             {
                 if (!_facingRight)
                     _anim.Play("metarig_Character_Switch_LeftRight");
@@ -55,15 +122,8 @@ namespace Amatorii_Controller
                     _anim.Play("metarig_Character_IdleRun_Right");
                 _facingRight = true;
             }
-            float animationLength = _anim.GetCurrentAnimatorStateInfo(0).length;
-
-            yield return new WaitForSeconds(animationLength);
-
-                if (_facingRight)
-                    _anim.Play("metarig_Character_RunningLoop_Right");
-                else
-                    _anim.Play("metarig_Character_RunningLoop_Left");
-            
+            yield return new WaitForSeconds(_anim.GetCurrentAnimatorStateInfo(0).length + _anim.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            _moving = true;
         }
     }
 }
